756690da8fb2054578bc65fc83227e96c3d0aa96
John Dio Lumacang <tcshs.lumacang.johndio.a@gmail.com>
2026-02-01 21:37:13 +0800

    adjusted level1_controller timeout to 0.5

--- scripts/level1_controller.gd
+++ scripts/level1_controller.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 func _ready() -> void:
 	# Wait a brief moment for the scene to fully initialize
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.5).timeout
 
 	# Automatically show the developer welcome message
 	if dialog_system:
